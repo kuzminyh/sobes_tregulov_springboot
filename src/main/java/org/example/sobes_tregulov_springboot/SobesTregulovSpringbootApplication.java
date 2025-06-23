5f6d8c4496d5bedacf22cb2ae0c7e2b90516f0e2
Sergey Kuzminykh <kuzminyhsa@gmail.com>
2025-06-24 06:32:10 +0800

    first commit

---
+++ src/main/java/org/example/sobes_tregulov_springboot/SobesTregulovSpringbootApplication.java
@@ -0,0 +1,13 @@
+package org.example.sobes_tregulov_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SobesTregulovSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SobesTregulovSpringbootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/sobes_tregulov_springboot/SobesTregulovSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.sobes_tregulov_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SobesTregulovSpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
